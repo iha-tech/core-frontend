b55b4bcb1c9e7b83480a01495a9f70614b7fab03
Gustavo Iha <gustavoiha@gmail.com>
2022-05-11 18:46:30 -0300

    feat: create Playground.vue

---
+++ src/views/Playground.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header class="playground__header">
+    </header>
+
+    <article>
+      <section class="playground__section-title">
+        <h1 class="playground__title">
+          Playground
+        </h1>
+
+        <p class="playground__subtitle">
+          Let's test some stuff!
+        </p>
+      </section>
+
+      <section class="playground__section-body">
+      </section>
+    </article>
+
+    <footer class="playground__footer">
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
