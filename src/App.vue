4976b4580ea086eb803ca4c26f81d4be49043433
Gustavo Iha <gustavoiha@gmail.com>
2022-05-15 16:52:23 -0300

    fix: remove h1 from App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,6 @@
           </template>
 
           <template #default> -->
-            <h1>This is Iha Tech! A representation of my own personal world in a digital form!</h1>
-    
             <component :is="Component"></component>
           <!-- </template>
         </suspense> -->
