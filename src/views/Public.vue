3a09c49b0d2989829bfc0d5de8daa5f77fd8dacf
Gustavo Iha <gustavoiha@gmail.com>
2022-05-15 17:38:48 -0300

    refactor: move animations to scss partial

--- src/views/Public.vue
+++ src/views/Public.vue
@@ -22,13 +22,5 @@
 </template>
 
 <style lang="scss" scoped>
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s ease;
-  }
-
-  .fade-enter-from,
-  .fade-leave-to {
-    opacity: 0;
-  }
+  @use '@/assets/partials/animations';
 </style>
