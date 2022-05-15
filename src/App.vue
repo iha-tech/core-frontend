438d9f46965f0a3a188bab21d99db6980aa6f466
Gustavo Iha <gustavoiha@gmail.com>
2022-05-15 12:58:51 -0300

    chore: adds router-vie with loader in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
+<script setup>
+  import ViewLoading from './components/ViewLoading.vue'
+</script>
+
 <template>
   <main>
-    <h1>This is Iha Tech! A representation of my own personal world in a digital form!</h1>
+    <router-view v-slot="{ Component }">
+      <suspense>
+        <template #fallback>
+          <view-loading />
+        </template>
+
+        <template #default>
+          <h1>This is Iha Tech! A representation of my own personal world in a digital form!</h1>
+  
+          <component :is="Component"></component>
+        </template>
+      </suspense>
+    </router-view>
   </main>
 </template>
