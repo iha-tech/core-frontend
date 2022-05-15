34aa95255317b410852df04d055107d1da2ccc98
Gustavo Iha <gustavoiha@gmail.com>
2022-05-15 12:39:09 -0300

    config: set history mode

--- src/config/router.js
+++ src/config/router.js
@@ -1,6 +1,7 @@
-import { createRouter } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from './routes'
 
 export default createRouter({
+  history: createWebHashHistory(),
   routes
 })
