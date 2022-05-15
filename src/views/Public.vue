fc457901ff562316b279bb86d3901e761c00a942
Gustavo Iha <gustavoiha@gmail.com>
2022-05-15 17:12:46 -0300

    fix: adds fade transition css

--- src/views/Public.vue
+++ src/views/Public.vue
@@ -5,18 +5,30 @@
 <template>
   <router-view v-slot="{ Component, route }">
     <transition name="fade" mode="out-in">
-      <!-- <suspense>
+      <suspense>
         <template #fallback>
           <view-loading />
         </template>
 
-        <template #default> -->
+        <template #default>
           <component
             :is="Component"
             :key="route.path"
           />
-        <!-- </template>
-      </suspense> -->
+        </template>
+      </suspense>
     </transition>
   </router-view>
 </template>
+
+<style>
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s ease;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
