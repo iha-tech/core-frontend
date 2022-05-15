75eb8bdcf283945a1125cdb3a9e27e3e1aa282aa
Gustavo Iha <gustavoiha@gmail.com>
2022-05-15 17:40:37 -0300

    refactor: App.vue imports animations from scss partial

--- src/App.vue
+++ src/App.vue
@@ -20,14 +20,6 @@
   </main>
 </template>
 
-<style>
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s ease;
-  }
-
-  .fade-enter-from,
-  .fade-leave-to {
-    opacity: 0;
-  }
+<style lang="scss" scoped>
+  @use '@/assets/partials/animations';
 </style>
